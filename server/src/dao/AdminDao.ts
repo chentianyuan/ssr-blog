9169e1e18b1746164f9c199b542fe6992ad5b297
chenty1 <chenty1@guahao.com>
2018-09-12 23:36:56 +0800

    server

--- server/src/dao/AdminDao.ts
+++ server/src/dao/AdminDao.ts
@@ -2,12 +2,20 @@
 
 import { Injectable } from '@decorators/di'
 import { getRepository as getRep } from 'typeorm'
-import '../entities/Admin'
+import Admin from '../entities/Admin'
 
 @Injectable()
 export default class AdminDao {
   findUserAcountByNameAndPwd (admin: string, password: string) {
     // 先通过getRepository方法获取到admin实体
-    return getRep
+    return getRep(Admin)
+    // 创建查询结构体
+    .createQueryBuilder()
+    // 查询语句
+    .select()
+    // 条件语句
+    .where('admin = :admin', { admin })
+    .andWhere('password = :password', { password })
+    .getOne()
   }
 }
