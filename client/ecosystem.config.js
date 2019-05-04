7b947ee2537c1cde469246aa204df1e28943911d
chenty1 <chenty1@guahao.com>
2019-05-04 18:02:02 +0800

    部署

---
+++ client/ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  apps : [{
+    name: 'blog',
+    script: '/server/index.js',
+
+    // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
+    // 远程服务器上的PM2参数配置
+    // args: 'one two', // 参数
+    instances: 1, // 实例数量
+    autorestart: true, // 自动启动
+    watch: false, // 监视模式
+    max_memory_restart: '1G', // 内存占用超过多少后重启实例
+    env: {
+      NODE_ENV: 'development'
+    },
+    env_production: {
+      NODE_ENV: 'production'
+    }
+  }],
+
+  deploy : {
+    production : {
+      user : 'root',
+      host : '49.97.110.222',
+      ref  : 'origin/master', // 远程git上要部署的分支
+      repo : 'https://github.com/chentianyuan/prerender.git',
+      path : '/usr/project',
+      'post-deploy' : 'npm install && pm2 reload ecosystem.config.js --env production'
+    }
+  }
+};
